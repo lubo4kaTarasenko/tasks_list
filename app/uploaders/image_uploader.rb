4c11fa11a2de096770fa0aa9ee6612fd5f33150f
luba.t <luba@t.me>
2018-11-26 19:33:11 +0200

    work on profile page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  	
+  include CarrierWave::MiniMagick
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -29,9 +31,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+     process resize_to_fit: [300, 300]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
